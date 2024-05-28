cdd45e79f6eb3722a19f4d61398dfd274ba7a3e4
Nicola Cucinella <cucinellanicola735@gmail.com>
2024-05-28 22:25:55 +0200

    Ho aggiunto il font sans-serif ma non è applicato ancora d'ovunque, ho aggiunto nel render il box contente ArtistProfile

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import GroupList from './components/GroupList';
+import ArtistProfile from './components/ArtistProfile';
 
 const groups = [{
   name: 'Group 1',
@@ -39,9 +40,29 @@ date: '2021-10-01'},
 {name: 'Ciccio',
 members: 12,
 type: 'cnocert',
-date: '2021-10-01'}]
+date: '2021-10-01'}];
 
 const darkTheme = createTheme({
+  typography: {
+    fontFamily: [
+      'sans-serif',
+    ].join(','),
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'sans-serif';
+          font-style: normal;
+          src: local('sans-serif'), local('sans-serif'),
+          url(https://fonts.gstatic.com/s/opensans/v26/mem8YaGs126MiZpBA-UFWJ0bf8pkAp6a.woff2) format('woff2');
+        }
+        body {
+          font-family: 'sans-serif';
+        }
+      `,
+    },
+  },
   palette: {
     mode: 'dark',
     primary: {
@@ -58,29 +79,40 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Box sx={{ display: 'flex' }}>
-          <Sidebar />
-          <Container sx={{ boxSizing: "border-box", overflow: "hidden" }} maxWidth="false">
-            <Box sx={{ position: 'relative', flexGrow: 1,  justifyContent: 'center', display:"flex"}}>
-                <img src={banner} alt='banner pubblicitario' style={{ width: '100%', maxHeight:"20vh", borderRadius: '12px', marginTop:"20px", maxWidth:"1100px", display:"flex" }} />
-            </Box>
-              <Box sx={{
-                overflowY: "scroll",
-                overflowX: "hidden", 
-                marginTop:"20px",
-                padding:"10px",
-                height:"73vh", 
-                minHeight:"70vh", 
-                //maxHeight:"75vh", 
-                //border: '4px solid #FF6D2E',
-                borderRadius: '12px', 
-                backgroundColor: '#332D2A',
-                
+      <CssBaseline />
+      <Box sx={{ display: 'flex' }}>
+        <Sidebar />
+        <Container sx={{ boxSizing: "border-box", overflow: "hidden" }} maxWidth="false">
+          <Box sx={{ position: 'relative', flexGrow: 1,  justifyContent: 'center', display:"flex"}}>
+              <img src={banner} alt='banner pubblicitario' style={{ width: '100%', maxHeight:"20vh", borderRadius: '12px', marginTop:"20px", maxWidth:"1100px", display:"flex" }} />
+          </Box>
+          <Box sx={{
+              overflowY: "scroll",
+              overflowX: "hidden", 
+              marginTop:"20px",
+              padding:"10px",
+              height:"73vh", 
+              minHeight:"70vh", 
+              //maxHeight:"75vh", 
+              //border: '4px solid #FF6D2E',
+              borderRadius: '12px', 
+              backgroundColor: '#332D2A',
+              
 
-              }}>
-            <GroupList groups={groups} />
-            <GroupList groups={groups} />
+            }}>
+          <GroupList groups={groups} />
+          <GroupList groups={groups} />
+          </Box>
+          <Box sx={{
+              overflowY: "scroll",
+              overflowX: "hidden", 
+              marginTop:"20px",
+              height:"73vh", 
+              minHeight:"70vh",
+              borderRadius: '12px', 
+              backgroundColor: '#332D2A',
+            }}>
+          <ArtistProfile />
           </Box>
         </Container>
       </Box>
