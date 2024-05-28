42b097b360030acae508c8a5c219fb54f4e9887d
Nicola Cucinella <cucinellanicola735@gmail.com>
2024-05-28 22:23:20 +0200

    Dati test artista aggiunti

---
+++ client/src/data.js
@@ -0,0 +1,28 @@
+const artistData = {
+    name: "Nayt",
+    monthlyListeners: 720000,
+    imageUrl: "link-to-image.jpg", // replace with the actual image URL
+    closeToYou: {
+      date: "NOV 25",
+      city: "Modugno",
+      day: "Sab",
+      time: "8 PM",
+      venue: "Demodè Club"
+    },
+    otherCities: [
+      { date: "10 NOV", city: "Parma", day: "Ven", time: "8 PM", venue: "Campus Industry Music" },
+      { date: "15 NOV", city: "Milano", day: "Mer", time: "8 PM", venue: "Alcatraz" },
+      { date: "16 NOV", city: "Padova", day: "Gio", time: "8 PM", venue: "Hall" },
+      { date: "10 NOV", city: "Parma", day: "Ven", time: "8 PM", venue: "Campus Industry Music" },
+      { date: "15 NOV", city: "Milano", day: "Mer", time: "8 PM", venue: "Alcatraz" },
+      { date: "16 NOV", city: "Padova", day: "Gio", time: "8 PM", venue: "Hall" },
+      { date: "10 NOV", city: "Parma", day: "Ven", time: "8 PM", venue: "Campus Industry Music" },
+      { date: "15 NOV", city: "Milano", day: "Mer", time: "8 PM", venue: "Alcatraz" },
+      { date: "16 NOV", city: "Padova", day: "Gio", time: "8 PM", venue: "Hall" },
+      { date: "10 NOV", city: "Parma", day: "Ven", time: "8 PM", venue: "Campus Industry Music" },
+      { date: "15 NOV", city: "Milano", day: "Mer", time: "8 PM", venue: "Alcatraz" },
+      
+    ]
+  };
+  
+  export default artistData;
