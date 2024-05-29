f301ee8df7b264f81922df509630f54fca077593
Nicola Cucinella <cucinellanicola735@gmail.com>
2024-05-29 12:35:42 +0200

    Ho pulito un po' la index

--- client/src/index.js
+++ client/src/index.js
@@ -10,70 +10,16 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import GroupList from './components/GroupList';
 import ArtistProfile from './components/ArtistProfile';
+import darkTheme from './theme';
 
-const groups = [{
-  name: 'Group 1',
-  members: 65,
-  type: 'cnocert',
-  date: '2021-10-01',
-}, {
-  name: 'Group 2',
-  members: 12,
-  type: 'cnocert',
-  date: '2021-10-01',
-},
-{name: 'Ciccio',
-members: 12,
-type: 'cnocert',
-date: '2021-10-01'},
-{
-  name: 'Group 1',
-  members: 65,
-  type: 'cnocert',
-  date: '2021-10-01',
-}, {
-  name: 'Group 2',
-  members: 12,
-  type: 'cnocert',
-  date: '2021-10-01',
-},
-{name: 'Ciccio',
-members: 12,
-type: 'cnocert',
-date: '2021-10-01'}];
-
-const darkTheme = createTheme({
-  typography: {
-    fontFamily: [
-      'sans-serif',
-    ].join(','),
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: `
-        @font-face {
-          font-family: 'sans-serif';
-          font-style: normal;
-          src: local('sans-serif'), local('sans-serif'),
-          url(https://fonts.gstatic.com/s/opensans/v26/mem8YaGs126MiZpBA-UFWJ0bf8pkAp6a.woff2) format('woff2');
-        }
-        body {
-          font-family: 'sans-serif';
-        }
-      `,
-    },
-  },
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#FF6D2E',
-    },
-    background: {
-      // default: '#332D2A',
-      paper: '#332D2A',
-    },
-  },
-});
+const groups = [
+  { name: 'Group 1', members: 65, type: 'cnocert', date: '2021-10-01' },
+  { name: 'Group 2', members: 12, type: 'cnocert', date: '2021-10-01' },
+  { name: 'Ciccio', members: 12, type: 'cnocert', date: '2021-10-01' },
+  { name: 'Group 1', members: 65, type: 'cnocert', date: '2021-10-01' },
+  { name: 'Group 2', members: 12, type: 'cnocert', date: '2021-10-01' },
+  { name: 'Ciccio', members: 12, type: 'cnocert', date: '2021-10-01' }
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -87,31 +33,16 @@ root.render(
               <img src={banner} alt='banner pubblicitario' style={{ width: '100%', maxHeight:"20vh", borderRadius: '12px', marginTop:"20px", maxWidth:"1100px", display:"flex" }} />
           </Box>
           <Box sx={{
-              overflowY: "scroll",
-              overflowX: "hidden", 
-              marginTop:"20px",
-              padding:"10px",
-              height:"73vh", 
-              minHeight:"70vh", 
-              //maxHeight:"75vh", 
-              //border: '4px solid #FF6D2E',
-              borderRadius: '12px', 
-              backgroundColor: '#332D2A',
-              
-
+              overflowY: "scroll", overflowX: "hidden", 
+              marginTop:"20px", padding:"10px",
+              height:"73vh", minHeight:"70vh",
+              borderRadius: '12px', backgroundColor: '#332D2A',
             }}>
           <GroupList groups={groups} />
           <GroupList groups={groups} />
           </Box>
-          <Box sx={{
-              overflowY: "scroll",
-              overflowX: "hidden", 
-              marginTop:"20px",
-              height:"73vh", 
-              minHeight:"70vh",
-              borderRadius: '12px', 
-              backgroundColor: '#332D2A',
-            }}>
+
+          <Box sx={{overflowY: "scroll", overflowX: "hidden", marginTop:"20px"}}>
           <ArtistProfile />
           </Box>
         </Container>
