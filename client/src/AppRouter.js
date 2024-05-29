b0bd0901e553a84f6ecb775a99b2a1e3a25f617d
Nicola Cucinella <cucinellanicola735@gmail.com>
2024-05-29 18:50:03 +0200

    nulla di speciale qui

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProfileCard from './components/ProfileCard';
 import ConcertCard from './components/ConcertCard';
 import GroupList from './components/GroupList';
 import NotFound from './components/NotFound';
+import ArtistProfile from './components/ArtistProfile';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<ProfileCard />} />
+        <Route path="/artist-profile" element={<ArtistProfile />} />
         <Route path="/concert" element={<ConcertCard />} />
         <Route path="/groups" element={<GroupList />} />
         <Route path="*" element={<NotFound />} />
