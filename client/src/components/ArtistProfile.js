f3602f92e2945d2716da70f08cdf21c8eab0918f
Nicola Cucinella <cucinellanicola735@gmail.com>
2024-05-29 18:51:32 +0200

    dati presi dal file json

--- client/src/components/ArtistProfile.js
+++ client/src/components/ArtistProfile.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Container, Box, Avatar, Typography, Button, Card, CardContent, Grid, IconButton, ListItem } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Container, Box, Avatar, Typography, Button, Card, CardContent, Grid, IconButton } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
